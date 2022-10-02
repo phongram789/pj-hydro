96b630065ad92e9e8452fd003f85236ce9059221
พงศ์ราม พึ่งบุญ ณ อยุธยา <73733731+phongram789@users.noreply.github.com>
2022-10-02 19:41:27 +0700

    update lib

---
+++ mqtt_esp32/flow.h
@@ -0,0 +1,47 @@
+#include<arduino.h>
+
+class waterflow{
+  private:  
+    int t_pin;
+    int X;
+    int Y;
+    float TIME = 0;
+    float FREQUENCY = 0;
+    float WATER = 0;
+    float TOTAL = 0;
+    float LS = 0;
+
+  public:
+    flowInput(int pin){
+      t_pin = pin;
+      pinMode(t_pin,INPUT_PULLUP);
+    }
+  
+    float get_flow(){
+      return digitalRead(t_pin);
+    }
+    calwaterflow(){
+      X = pulseIn(t_pin, HIGH);
+      Y = pulseIn(t_pin, LOW);
+      TIME = X + Y;
+      FREQUENCY = 1000000/TIME;
+      WATER = FREQUENCY/7.5; // 7.5 is value of calibration
+      LS = WATER/60;
+      if(FREQUENCY >= 0){
+        if(isinf(FREQUENCY)){
+         Serial.println("Not flowing");
+         //Serial.println(TIME);
+         // TIME/100
+         }
+         else{
+          Serial.println("Water is flowing");
+          //TOTAL = TOTAL + LS;
+          //Serial.println(FREQUENCY);
+          //Serial.println(WATER);
+          //Serial.println(TOTAL);
+          // TOTAL L/H
+          }
+       }
+    }
+  
+};//end of class
